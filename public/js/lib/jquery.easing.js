5845cda1cde022154c52db41828058eb8760ea73
arthur.lecompte <arthur.lecompte14@gmail.com>
2024-04-08 11:22:36 +0200

    refactor

--- public/js/lib/jquery.easing.js
+++ public/js/lib/jquery.easing.js
@@ -8,11 +8,11 @@
 
 (function (factory) {
 	if (typeof define === "function" && define.amd) {
-		define(['jquery'], function ($) {
+		define(['public/js/lib/jquery'], function ($) {
 			return factory($);
 		});
 	} else if (typeof module === "object" && typeof module.exports === "object") {
-		exports = factory(require('jquery'));
+		exports = factory(require('public/js/lib/jquery'));
 	} else {
 		factory(jQuery);
 	}
